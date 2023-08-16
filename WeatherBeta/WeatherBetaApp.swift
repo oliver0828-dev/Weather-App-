9dcb7012888cdf339aac4309d2d45e2c0041a95d
Oliver Park <oliverco0828@gmail.com>
2023-08-16 21:28:30 +0900

    Initial Commit

---
+++ WeatherBeta/WeatherBetaApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherBetaApp.swift
+//  WeatherBeta
+//
+//  Created by Oliver Park on 2023/08/16.
+//
+
+import SwiftUI
+
+@main
+struct WeatherBetaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
